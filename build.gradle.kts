3efd245879c8420eca4fb8f7a677bb61dfb01e92
Richter3766 <97567615+Richter3766@users.noreply.github.com>
2025-01-10 15:31:49 +0900

    chore: 프로젝트 의존성 설정
    테스트: kotest + mockK
    코루틴, redis, webflux 활용

--- build.gradle.kts
+++ build.gradle.kts
@@ -19,20 +19,38 @@ repositories {
 }
 
 dependencies {
-	implementation("org.springframework.boot:spring-boot-starter-data-redis-reactive")
+	// webflux
 	implementation("org.springframework.boot:spring-boot-starter-webflux")
 
+	// jackson
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 	implementation("io.projectreactor.kotlin:reactor-kotlin-extensions")
 
+	// reflect
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
-	implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor")
 
+	// test
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testImplementation("io.projectreactor:reactor-test")
 	testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
-
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+
+	// Kotest
+	testImplementation("io.kotest:kotest-runner-junit5:5.9.1")
+	testImplementation("io.kotest:kotest-assertions-core:5.9.1")
+	testImplementation("io.kotest.extensions:kotest-extensions-spring:1.3.0")
+
+	// MockK
+	testImplementation("io.mockk:mockk:1.13.12")
+	testImplementation("com.ninja-squad:springmockk:4.0.2")
+
+	// Kotlin Coroutine
+	implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactive:1.9.0")
+	implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.9.0")
+	implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor:1.9.0")
+
+	// redis
+	implementation("org.springframework.boot:spring-boot-starter-data-redis-reactive")
 }
 
 kotlin {
