771ce3e220cf5ae8e7c73a564c4ea547ba095155
Richter3766 <97567615+Richter3766@users.noreply.github.com>
2025-01-15 15:03:35 +0900

    fix: ID 생성기 timestamp 수정
    현재 시간 밀리 초를 그대로 쓰지 않고,
    현재 시간에 기원 시각 2025.01.01 기준 밀리 초를
    뺀 값으로 timestamp로 활용하도록 수정

--- src/main/kotlin/com/example/url_shortener/service/util/UniqueIdGenerator.kt
+++ src/main/kotlin/com/example/url_shortener/service/util/UniqueIdGenerator.kt
@@ -2,8 +2,13 @@ package com.example.url_shortener.service.util
 
 import kotlinx.coroutines.sync.Mutex
 import kotlinx.coroutines.sync.withLock
+import java.time.LocalDateTime
+import java.time.ZoneOffset
 
 class SnowflakeGenerator(private val dataCenterId: Long, private val serverId: Long) {
+    private val specificDateTime = LocalDateTime.of(2025, 1, 1, 0, 0, 0)
+    private val epochTime = specificDateTime.toInstant(ZoneOffset.UTC).toEpochMilli()
+
     private var lastTimestamp: Long = -1
     private var sequence: Long = 0
     private val mutex = Mutex()
@@ -25,9 +30,8 @@ class SnowflakeGenerator(private val dataCenterId: Long, private val serverId: L
             }
 
             lastTimestamp = timestamp
-
             var id = 0L
-            id = id or (timestamp shl 22)
+            id = id or ((timestamp - epochTime) shl 22)
             id = id or (dataCenterId shl 17)
             id = id or (serverId shl 12)
             id = id or sequence
